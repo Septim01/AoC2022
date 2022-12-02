459f7fde458f96a9083971c7c1267fbf15885c6c
Matúš Mitro <90218267+Septim01@users.noreply.github.com>
2022-12-02 13:42:36 +0100

    Update and rename a to Rock_paper_scissors.py

---
+++ src/day2/Rock_paper_scissors.py
@@ -0,0 +1,67 @@
+tmp = []
+x = 0
+y = 0
+win = 6
+draw = 3
+loss = 0
+
+def compare_first_part(arr):
+    score = 0
+    if arr[0] == "A":
+        if arr[1] == "X":
+            score += draw + 1
+        elif arr[1] == "Y":
+            score += win + 2
+        elif arr[1] == "Z":
+            score += loss + 3
+    elif arr[0] == "B":
+        if arr[1] == "X":
+            score += loss + 1
+        elif arr[1] == "Y":
+            score += draw + 2
+        elif arr[1] == "Z":
+            score += win + 3
+    elif arr[0] == "C":
+        if arr[1] == "X":
+            score += win + 1
+        elif arr[1] == "Y":
+            score += loss + 2
+        elif arr[1] == "Z":
+            score += draw + 3
+    return score
+
+
+def compare_second_part(arr):
+    score = 0
+    if arr[0] == "A":
+        if arr[1] == "X":
+            score += loss + 3
+        elif arr[1] == "Y":
+            score += draw + 1
+        elif arr[1] == "Z":
+            score += win + 2
+    elif arr[0] == "B":
+        if arr[1] == "X":
+            score += loss + 1
+        elif arr[1] == "Y":
+            score += draw + 2
+        elif arr[1] == "Z":
+            score += win + 3
+    elif arr[0] == "C":
+        if arr[1] == "X":
+            score += loss + 2
+        elif arr[1] == "Y":
+            score += draw + 3
+        elif arr[1] == "Z":
+            score += win + 1
+    return score
+
+with open("inp.txt", "r") as f:
+    for line in f:
+        tmp.append(line[0])
+        tmp.append(line[2])
+        x += compare_first_part(tmp)
+        y += compare_second_part(tmp)
+        tmp = []
+
+print(x, y)
